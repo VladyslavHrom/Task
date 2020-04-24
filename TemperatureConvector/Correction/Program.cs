781c95a90bc0d79d05afaf7327d24e64d1dba837
Grom <vladgr.vg@gmail.com>
2020-04-24 14:09:46 +0300

    1

---
+++ TemperatureConvector/Correction/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Correction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = Console.ReadLine();
+            int length = sentence.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                sentence = sentence.Replace("  ", " ");
+
+            }
+            Console.WriteLine($"{sentence}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TemperatureConvector/TemperatureConvector/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TemperatureConvector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Celsius = new int[201];
+            int[] Kelvins = new int[201];
+            double[] Fahrenheits = new double[201];
+            Celsius[0] = -100;
+            Kelvins[0] = 173;
+            Fahrenheits[0] = -148;
+            
+            for (int i = 1; i < Celsius.Length; i++)
+            {
+                Celsius[i] = Celsius[i - 1] + 1;
+            }
+            for (int i = 1; i < Kelvins.Length; i++)
+            {
+                Kelvins[i] = Kelvins[i - 1] + 1;
+            }
+            for (int i = 1; i < Fahrenheits.Length; i++)
+            {
+                Fahrenheits[i] = Fahrenheits[i - 1] + 1.8;
+            }
+            Console.WriteLine("Celsius \t kelvins \t fahrenheits");
+            for (int i = 0; i < Celsius.Length; i++)
+            {
+                Console.WriteLine($"{Celsius[i]} \t\t {Kelvins[i]} \t {Fahrenheits[i]}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
